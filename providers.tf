7c67c7c94c0e0c58d9174a00017525905d8dd423
bhaskarmazu <bhaskarmazu@gmail.com>
2026-08-06 12:50:09 -0400

    added S3 backend for state in provider.tf

--- providers.tf
+++ providers.tf
@@ -1,3 +1,18 @@
+# terraform {
+#   required_version = ">= 1.9"
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.0"
+#     }
+#   }
+# }
+
+# provider "aws" {
+#   region = "us-west-2"
+# }
+#
+#provider with S3 backend for state management
 terraform {
   required_version = ">= 1.9"
   required_providers {
@@ -5,6 +20,14 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+  }
+
+  backend "s3" {
+    bucket         = "terraform-learning-state-048280526216"
+    key            = "terraform-learning/terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "terraform-learning-locks"
+    encrypt        = true
   }
 }
 
